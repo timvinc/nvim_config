acd790ccfc3baf0d389985b96a23e41c53a30369
Tim Vincent <tim.vinc@gmail.com>
2024-02-26 19:38:47 -0800

    setting up neogit

--- init.lua
+++ init.lua
@@ -1,4 +1,38 @@
 return {
+  plugins={
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.pack.rust" },
+    { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+    { import = "astrocommunity.completion.copilot-lua" },
+    { -- further customize the options set by the community
+      "zbirenbaum/copilot.lua",
+      opts = {
+        suggestion = {
+          keymap = {
+            accept = "<C-l>",
+            accept_word = false,
+            accept_line = false,
+            next = "<C-.>",
+            prev = "<C-,>",
+            dismiss = "<C/>",
+          },
+        },
+      },
+    },
+    {
+      "NeogitOrg/neogit",
+      dependencies = {
+        "nvim-lua/plenary.nvim",         -- required
+        "sindrets/diffview.nvim",        -- optional - Diff integration
+
+        -- Only one of these is needed, not both.
+        "nvim-telescope/telescope.nvim", -- optional
+        "ibhagwan/fzf-lua",              -- optional
+      },
+      config = true,
+    },
+    { 'echasnovski/mini.nvim', version = false },
+  },
   -- Configure AstroNvim updates
   updater = {
     remote = "origin", -- remote to use
@@ -18,7 +52,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "everforest",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
